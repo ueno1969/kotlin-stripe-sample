620cc04f2e7577c9cbfd025984fe9712da51fe7b
Kazuhiko UENO <k-ueno@fujisan.co.jp>
2022-12-22 14:35:20 +0900

    Controller層を追加

---
+++ src/main/kotlin/com/example/contoroller/contents/ContentController.kt
@@ -0,0 +1,11 @@
+data class Content(val id: Int, val name: String, val price: Int)
+
+class ContentController() {
+    fun list(): List<Content> {
+        return listOf(
+            Content(1, "赤ペン", 100),
+            Content(2, "青ペン", 200),
+            Content(3, "黄ペン", 300),
+        )
+    }
+}
